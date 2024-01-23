f5abdaf72f78c3f6e52a04d602bf2a2586789bfa
Ansh Infotech <anshinfotech1@gmail.com>
2024-01-23 23:17:46 +0530

    Responsive

--- client/src/pages/home/TestReview.jsx
+++ client/src/pages/home/TestReview.jsx
@@ -8,36 +8,43 @@ const Testimonial = () => {
 
   const openModal = () => {
     setIsModalOpen(true);
-  }
+  };
 
   const closeModal = () => {
     setIsModalOpen(false);
-  }
+  };
 
   return (
-    <div className="relative">
-      <div className="w-full h-[600px] relative">
-        <video src="/muscleshark/video.mp4" autoPlay loop className="object-cover w-full h-full"></video>
-        <div className="absolute top-6 left-0 right-0 flex flex-col items-center justify-center">
-          <div className="text-center">
-            <h1 className="text-4xl font-bold mb-2" style={{ fontFamily: "Cinzel" }}>
-              Muscle<span className="text-amber-400">Sharks</span>
-            </h1>
-            <p className="text-lg text-white w-full md:w-[300px] mb-4">
-              Your Trusted Destination for Premium Gym Supplements
-            </p>
-          </div>
-          <div className="button-container mt-4">
-            <button className="custom-button w-full md:w-auto max-w-xs md:max-w-none" onClick={openModal}>
+    <div className="relative w-full h-[600px]">
+      <video
+        src="/muscleshark/video.mp4"
+        autoPlay
+        loop
+        muted
+        className="object-cover w-full h-full"
+      ></video>
+
+      <div className="absolute top-32 left-0 right-0 bottom-0 flex items-center justify-center text-center text-white z-10">
+        <div>
+          <h1 className="text-4xl font-bold mb-2" style={{ fontFamily: "Cinzel" }}>
+            Muscle<span className="text-amber-400">Sharks</span>
+          </h1>
+          <p className="text-lg mb-4">
+            Your Trusted Destination for Premium Gym Supplements
+          </p>
+
+          <div className="button-container">
+            <button className="custom-button" onClick={openModal}>
               GET YOUR COUPON
             </button>
           </div>
         </div>
       </div>
+
       {isModalOpen && <DiscountModal closeModal={closeModal} />}
       <ShopByCategory />
     </div>
   );
-}
+};
 
 export default Testimonial;
