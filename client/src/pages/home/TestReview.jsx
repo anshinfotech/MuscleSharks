f8c967f4bd25669b339ba9b63e89d05aec4efa60
Ansh Infotech <anshinfotech1@gmail.com>
2024-01-23 22:44:39 +0530

    responsive

--- client/src/pages/home/TestReview.jsx
+++ client/src/pages/home/TestReview.jsx
@@ -17,18 +17,18 @@ const Testimonial = () => {
   return (
     <div className="relative">
       <div className="w-full h-[600px] relative">
-        <video src="/muscleshark/video.mp4"  autoPlay loop className="object-cover w-full h-full"></video>
-        <div className="absolute items-center flex justify-around w-[100%] top-6 ">
-          <div className="">
+        <video src="/muscleshark/video.mp4" autoPlay loop className="object-cover w-full h-full"></video>
+        <div className="absolute items-center flex flex-col md:flex-row justify-around w-full top-6 mt-4">
+          <div className="text-center md:text-left">
             <h1 className="text-4xl font-bold mb-2" style={{ fontFamily: "Cinzel" }}>
               Muscle<span className="text-amber-400">Sharks</span>
             </h1>
-            <p className="text-lg text-white  w-[300px] mb-4">
+            <p className="text-lg text-white w-full md:w-[300px] mb-4">
               Your Trusted Destination for Premium Gym Supplements
             </p>
           </div>
-          <div className="button-container">
-            <button className="custom-button" onClick={openModal}>
+          <div className="button-container mt-4 md:mt-0">
+            <button className="custom-button w-full md:w-auto max-w-xs md:max-w-none" onClick={openModal}>
               GET YOUR COUPON
             </button>
           </div>
