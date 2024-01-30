9d25a2daf305af8c609899409d64d8e525d95051
Ansh Infotech <anshinfotech1@gmail.com>
2024-01-30 10:52:24 +0530

    db

--- server/database.js
+++ server/database.js
@@ -1,11 +1,12 @@
 const mongoose =require("mongoose");
 
 const dbConnection=async()=>{
+    
     try {
         // AnsukXqdnhrXUxy5
-        // const db=await mongoose.connect("mongodb://localhost:27017/gym");
-        const db=await mongoose.connect("mongodb+srv://muscleshark:muscleshark@cluster0.ye4umoz.mongodb.net/");
-        console.log("database is Connected  :)");
+        const db=await mongoose.connect("mongodb://localhost:27017/gym");
+        // await mongoose.connect("mongodb+srv://musclesharks:Muscle%40sharks07@atlascluster.0xem6uo.mongodb.net/musclesharks");
+        console.log("database is Connected");
     } catch (error) {
         console.log("Failed to Connect DataBase",error);
     }
